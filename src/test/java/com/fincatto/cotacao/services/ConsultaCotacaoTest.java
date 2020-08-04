daad7f4d2abc30f11be65f2243cc46bbb398d1ea
Felippe <fsilva@wmixvideo.com.br>
2020-08-04 15:07:57 -0300

    Migrado para junit5

--- src/test/java/com/fincatto/cotacao/services/ConsultaCotacaoTest.java
+++ src/test/java/com/fincatto/cotacao/services/ConsultaCotacaoTest.java
@@ -1,8 +1,9 @@
 package com.fincatto.cotacao.services;
 
 import com.fincatto.cotacao.model.Moeda;
-import org.junit.Assert;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,31 +13,31 @@ public class ConsultaCotacaoTest {
     @Test
     public void testDeveRetornarACotacaoDeVendaPorData() throws Exception {
         final LocalDate data = LocalDate.of(2015, 7, 27);
-        Assert.assertEquals(new BigDecimal("3.35570"), new ConsultaCotacao().getCotacao(Moeda.USD, data).getValorVenda());
+        Assertions.assertEquals(new BigDecimal("3.35570"), new ConsultaCotacao().getCotacao(Moeda.USD, data).getValorVenda());
     }
 
     @Test
     public void testDeveRetornarACotacaoDeCompraPorData() throws Exception {
         final LocalDate data = LocalDate.of(2015, 7, 27);
-        Assert.assertEquals(new BigDecimal("3.35510"), new ConsultaCotacao().getCotacao(Moeda.USD, data).getValorCompra());
+        Assertions.assertEquals(new BigDecimal("3.35510"), new ConsultaCotacao().getCotacao(Moeda.USD, data).getValorCompra());
     }
 
     @Test
     public void testDeveRetornarNuloQuandoForFeriado() throws Exception {
         final LocalDate data = LocalDate.of(2015, 6, 4);
-        Assert.assertNull(new ConsultaCotacao().getCotacao(Moeda.USD, data));
+        Assertions.assertNull(new ConsultaCotacao().getCotacao(Moeda.USD, data));
     }
 
     @Test
     public void testDeveRetornarNuloQuandoForFinalDeSemana() throws Exception {
         final LocalDate data = LocalDate.of(2015, 7, 5);
-        Assert.assertNull(new ConsultaCotacao().getCotacao(Moeda.USD, data));
+        Assertions.assertNull(new ConsultaCotacao().getCotacao(Moeda.USD, data));
     }
 
     @Test
     public void testDeveRetornarNuloQuandoDataForPosteriorAHoje() throws Exception {
         final LocalDate data = LocalDate.now().plusDays(1);
-        Assert.assertNull(new ConsultaCotacao().getCotacao(Moeda.USD, data));
+        Assertions.assertNull(new ConsultaCotacao().getCotacao(Moeda.USD, data));
     }
 
     @Test
@@ -44,7 +45,7 @@ public class ConsultaCotacaoTest {
         final LocalDate data = LocalDate.of(2015, 9, 18);
         final ConsultaCotacao consultaCotacao = new ConsultaCotacao();
         for (Moeda moeda : Moeda.values()) {
-            Assert.assertNotNull(consultaCotacao.getCotacao(moeda, data));
+            Assertions.assertNotNull(consultaCotacao.getCotacao(moeda, data));
         }
     }
 }
